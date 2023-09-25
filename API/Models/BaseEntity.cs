928a853d9137f85b99e5aa908ace21b5c6ee4c83
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-25 14:26:11 +0700

    Create BaseEntity.cs

---
+++ API/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models
+{
+    public class BaseEntity
+    {
+        public Guid Guid { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
