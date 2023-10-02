65ad690ff5df4b66f4473b28429b1029ca009f68
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-10-02 10:38:47 +0700

    Add CRUD HTTP AccountRole Models

---
+++ API/Contracts/IAccountRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+
+namespace API.Contracts;
+
+public interface IAccountRoleRepository
+{
+    IEnumerable<AccountRole> GetAll();
+    AccountRole? GetByGuid(Guid guid);
+    AccountRole? Create(AccountRole accountRole);
+    bool Update(AccountRole accountRole);
+    bool Delete(AccountRole accountRole);
+}
---
+++ API/Controllers/AccountRoleController.cs
@@ -0,0 +1,90 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountRoleController : ControllerBase
+{
+    private readonly IAccountRoleRepository _accountRoleRepository;
+
+    public AccountRoleController(IAccountRoleRepository accountRoleRepository)
+    {
+        _accountRoleRepository = accountRoleRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var result = _accountRoleRepository.GetAll();
+        if (!result.Any())
+        {
+            return NotFound("Data Not Found");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("{guid}")]
+    public IActionResult GetByGuid(Guid guid)
+    {
+        var result = _accountRoleRepository.GetByGuid(guid);
+        if (result is null)
+        {
+            return NotFound("Id Not Found");
+        }
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult Create(AccountRole accountRole)
+    {
+        var result = _accountRoleRepository.Create(accountRole);
+        if (result is null)
+        {
+            return BadRequest("Failed to create data");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPut("{guid}")]
+    public IActionResult Update(Guid guid, AccountRole accountRole)
+    {
+        var existingAccountRole = _accountRoleRepository.GetByGuid(guid);
+        if (existingAccountRole == null)
+        {
+            return NotFound("AccountRole not found");
+        }
+
+        // Update existingAccountRole properties with values from the provided 'accountRole' object
+        existingAccountRole.AccountGuid = accountRole.AccountGuid;
+        existingAccountRole.RoleGuid = accountRole.RoleGuid;
+
+        if (_accountRoleRepository.Update(existingAccountRole))
+        {
+            return Ok(existingAccountRole);
+        }
+
+        return BadRequest("Failed to update AccountRole");
+    }
+
+    [HttpDelete("{guid}")]
+    public IActionResult Delete(Guid guid)
+    {
+        var existingAccountRole = _accountRoleRepository.GetByGuid(guid);
+        if (existingAccountRole == null)
+        {
+            return NotFound("AccountRole not found");
+        }
+
+        if (_accountRoleRepository.Delete(existingAccountRole))
+        {
+            return Ok("AccountRole deleted successfully");
+        }
+
+        return BadRequest("Failed to delete AccountRole");
+    }
+}
